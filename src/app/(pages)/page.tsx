ab5329e9009042bab807cfc71c53b47bf8644912
emreertugrl <emrertugrl@gmail.com>
2024-12-04 12:51:18 +0300

    hero components completed

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,5 +1,14 @@
+import Brands from "../components/Brands";
+import Hero from "../components/Hero";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+      <Brands />
+      <h1>List</h1>
+    </div>
+  );
 };
 
 export default Home;
