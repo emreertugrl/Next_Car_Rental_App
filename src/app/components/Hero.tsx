b1e48cc5bf092a863dae06ad3327296f32719f60
emreertugrl <emrertugrl@gmail.com>
2024-12-04 13:15:59 +0300

    brands components defined to main

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,7 @@ import { models } from "./../utils/contants";
 const Hero = () => {
   return (
     <div className="h-[80vh] l-gradient grid place-items-center p-5  bg-center bg-cover text-white bg-[#00000084]">
-      <div className="text-center flex flex-col gap-8">
+      <div className="text-center flex flex-col gap-8 mt-10">
         <p>Yakınındaki kiralık araçları keşfet</p>
 
         <h1 className="text-4xl md:text-5xl font-bold">
